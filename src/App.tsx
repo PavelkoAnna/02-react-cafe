210b986c9741e7a55b81e308a6a5d82302e565c4
Anna <anyapavelko24@gmail.com>
2026-07-05 17:18:27 +0300

    Add cafe info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import CafeInfo from "./components/CafeInfo/CafeInfo";
 import css from "./App.module.css";
 export default function App() {
-  return <div className={css.app}></div>;
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
 }
